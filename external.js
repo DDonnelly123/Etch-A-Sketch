d03cd09082fa5cd88686633268255e785ead51eb
Darragh Donnelly <darraghsfu@gmail.com>
2023-10-18 10:09:09 -0700

    feat: add shader button/ effect for the etch-a-sketch, completed project

--- external.js
+++ external.js
@@ -8,6 +8,7 @@ output.innerHTML = `${slider.value} x ${slider.value}`;
 
 const blackMode = document.querySelector('#blackMode')
 const rainbowMode = document.querySelector('#rainbowMode')
+const shaderMode = document.querySelector('#shaderMode')
 const eraserMode = document.querySelector('#eraserMode')
 const clearMode = document.querySelector('#clearMode')
 
@@ -28,7 +29,9 @@ function createGrid(playerChoice) {
              cell.style.backgroundColor = getRandomColor();
             } else if (currentMode === "eraser") {
               cell.style.backgroundColor = "white"
-            } 
+            } else if (currentMode === "shader") {
+              cell.style.backgroundColor = darkenPencilColor(cell);
+            }
           });
 
             cell.style.width = cellSize + 'px';
@@ -45,7 +48,6 @@ function createGrid(playerChoice) {
 
 };  
     
-
 
 
 createGrid(playerChoice);
@@ -82,6 +84,10 @@ rainbowMode.addEventListener("click", () => {
   currentMode = "rainbow"
 });
 
+shaderMode.addEventListener("click", () => {
+  currentMode = "shader"
+})
+
 eraserMode.addEventListener("click", () => {
   currentMode = "eraser"
 });
@@ -95,4 +101,18 @@ function getRandomColor() {
   }
   return color;
 };
+
+
+function darkenPencilColor(cell) {
+  let originalColor = window.getComputedStyle(cell).backgroundColor;
+  let currentOpacity = originalColor.match(/[\d.]+/g)[3] || 0.0; // Extract the current opacity
+
+  let newOpacity = parseFloat(currentOpacity)
+
+  if (newOpacity < 0.9 && cell.style.backgroundColor != "black") {
+    cell.style.backgroundColor = `rgba(0, 0, 0, ${newOpacity + 0.1})`;
+  } else if ( cell.style.backgroundColor = `rgb(0, 0, 0)`) {
+    cell.style.backgroundColor = "black"
+  }
+}
 
